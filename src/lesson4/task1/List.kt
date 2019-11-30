4e4e0c227fbc0fd62b0a10e898051dda69aeb58b
zina-frid <friday.zina@gmail.com>
2019-11-30 19:20:41 +0300

    ...

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -195,7 +195,7 @@ fun accumulate(list: MutableList<Int>): MutableList<Int> {
 fun factorize(n: Int): List<Int> {
     val result = mutableListOf<Int>()
     var insteadN = n
-    for (i in minDivisor(n)..n) {
+    for (i in minDivisor(n)..sqrt(n.toDouble()).toInt()) {
         while (insteadN % i == 0) {
             result.add(i)
             insteadN /= i
